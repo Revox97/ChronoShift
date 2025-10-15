dea69e701bceec342fec543972dee57efe816ba4
Leopold Pohlmann <Leopold.Pohlmann@gmx.net>
2025-10-15 21:04:12 +0200

    add: initial project

---
+++ ChronoShift/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ChronoShift.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ ChronoShift/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ChronoShift.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
